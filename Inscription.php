9fdccd70efc70023d74d7ff5d71b8fe5ae6e47f2
Creepy4Bbby <82474796+Creepy4Bbby@users.noreply.github.com>
2022-02-09 21:00:08 +0100

    ,

---
+++ Inscription.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+
+
+
+?>
+
+<html>
+    <header>
+    <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="assets/css/co.css" />
+</header>
+        <body>
+        <div id="container">
+            <!-- zone de connexion -->
+            
+            <form action="Connection.php" method="POST">
+                <h1>Inscription</h1>
+
+
+
+                </form>
+        </div>
+    </body>
+</html>
--- Verification.php
+++ Verification.php
@@ -5,7 +5,7 @@ if(isset($_POST['username']) && isset($_POST['password']))
     // connexion à la base de données
     $db_username = 'root';
     $db_password = '';
-    $db_name = 'test';
+    $db_name = 'Gens';
     $db_host = 'localhost';
     $db_port = 3307;
     // $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
